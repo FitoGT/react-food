9044b863ccd8a0d272ea3b9956d4ef995190932e
Adolfo <adolfo.guevara.11@gmail.com>
2023-09-07 09:49:08 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Meals />
+    </>
+  );
+}
+
+export default App;
